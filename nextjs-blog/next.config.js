7c7cb964aed6825ee1b234b7bdb21a576a08b5fd
Denis Morozov <den.morozov@gmail.com>
2023-02-07 08:14:10 -0800

    Example stack with .NET core rest and next.js

---
+++ nextjs-blog/next.config.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
+      },
+    ];
+  }
+}
+
+module.exports = nextConfig;
