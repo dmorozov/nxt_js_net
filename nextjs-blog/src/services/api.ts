f3eaf94de81819ee798ad9cf48329e90bd920238
Denis Morozov <den.morozov@gmail.com>
2023-02-11 11:54:57 -0800

    fix lint

--- nextjs-blog/src/services/api.ts
+++ nextjs-blog/src/services/api.ts
@@ -5,9 +5,9 @@ const api = axios.create({
   timeout: 5000,
 });
 
-export const getPosts = (): string => {
+export const getPosts = (): Promise<string> => {
   /**
    * make your request here
    */
-  return '';
+  return api.get('https://some.url/api').then((response) => response.data);
 };
